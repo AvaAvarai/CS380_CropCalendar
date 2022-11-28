baa376d9d800a806ed7cc783f2586a96918be760
Koehl-Bussert <114447674+Koehl-Bussert@users.noreply.github.com>
2022-11-27 18:54:49 -0800

    Add files via upload

--- src/calendar/CalendarController.java
+++ src/calendar/CalendarController.java
@@ -1,11 +1,7 @@
 package application;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
