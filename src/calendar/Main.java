8e374032122fd153956d0c05b14733a210190ac9
AvaAvarai <alice.williams.tech@gmail.com>
2022-11-27 18:12:49 -0800

    Add icon and window title

--- src/calendar/Main.java
+++ src/calendar/Main.java
@@ -4,17 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/calendar/Calendar.fxml"));
 			Scene scene = new Scene(root,700,700);
 			scene.getStylesheets().add(getClass().getResource("calendar.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			stage.getIcons().add(new Image(getClass().getResourceAsStream( "icon.png" )));
+			stage.setTitle("Crop Calendar");
+			stage.setScene(scene);
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
